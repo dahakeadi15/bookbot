ed72fc03c3b6819e32cf8f1c91cc633ff97f6f05
Aditya Dahake <dahakeadi15@gmail.com>
2025-02-02 18:41:26 +0530

    get count of each character in the text

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
     num_words = get_word_count(text)
-    print(num_words)
+    num_chars = get_character_count(text)
+    print(num_chars)
 
 
 def get_book_text(path):
@@ -12,6 +13,19 @@ def get_book_text(path):
 
 def get_word_count(text):
     return len(text.split())
+
+
+def get_character_count(text):
+    chars = {}
+
+    for char in text:
+        lower_c = char.lower()
+        if lower_c in chars:
+            chars[lower_c] += 1
+        else:
+            chars[lower_c] = 1
+
+    return chars
 
 
 main()
