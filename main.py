656622277333430008bbd8cc0cf3733c8822b3a0
Aditya Dahake <dahakeadi15@gmail.com>
2025-07-03 17:16:40 +0530

    read file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(file_path: str) -> str:
+    file_contents = ""
+    with open(file_path) as file:
+        file_contents = file.read()
+
+    return file_contents
+
+
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+
+
+if __name__ == "__main__":
+    main()
