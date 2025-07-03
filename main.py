7d633937c504fbf1c2b768fbc8b12ab4aa414b21
Aditya Dahake <dahakeadi15@gmail.com>
2025-07-03 18:01:45 +0530

    print report and take arguments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_words, count_chars
+import sys
+from stats import count_words, count_chars, sort_counter_dict
 
 
 def get_book_text(file_path: str) -> str:
@@ -10,13 +11,28 @@ def get_book_text(file_path: str) -> str:
 
 
 def main():
-    book_contents = get_book_text("./books/frankenstein.txt")
+    inputs = sys.argv
 
+    if len(inputs) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = inputs[1]
+
+    book_contents = get_book_text(file_path)
     num_words = count_words(book_contents)
-    print(f"{num_words} words found in the document")
-
     char_count = count_chars(book_contents)
-    print(char_count)
+
+    # Print Report
+    print("============ BOOKBOT ============")
+    print(f"Analysing book found at {file_path}...")
+    print("---------- Word Count -----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char in sort_counter_dict(char_count):
+        if char["char"].isalpha():
+            print(f"{char['char']}: {char['count']}")
+    print("============= END ===============")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -10,4 +10,10 @@ def count_chars(string:str) -> dict:
 
     return counter
 
-print(count_chars("hello there"))
+
+def sort_counter_dict(unsorted_counter: dict) -> list:
+    sorted_counter_list = [{"char": char, "count": count} for char, count in unsorted_counter.items()]
+    sorted_counter_list.sort(reverse=True, key=lambda x: x["count"])
+
+    return sorted_counter_list
+
